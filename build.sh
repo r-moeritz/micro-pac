390d96de81e82dda404b0c68941e83f3171b8ad7
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2025-02-02 23:21:47 +1000

    Remove spurious sei from readjoy2

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ NAME=micropac64
 mkdir -p ./build
 
 vasm6502_oldstyle -cbm-prg -Fbin -chklabels -nocase -dotdir \
-                  src/startup.asm -o build/startup.prg -L "./build/$NAME.lst" \
-    && exomizer sfx sys -t 64 -o "build/$NAME.prg" ./build/startup.prg
+                  src/startup.asm -o build/$NAME.bin -L build/$NAME.lst \
+    && exomizer sfx sys -t 64 -o build/$NAME.prg build/$NAME.bin
